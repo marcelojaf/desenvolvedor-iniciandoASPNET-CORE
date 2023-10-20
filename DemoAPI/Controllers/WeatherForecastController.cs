004e7a213e3df46576cabd7ca5a231dad672f854
Marcelo Amador <marcelo@cklabs.dev>
2023-10-20 11:13:16 -0300

    Web API finalizado

---
+++ DemoAPI/Controllers/WeatherForecastController.cs
@@ -0,0 +1,47 @@
+using DemoAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [HttpGet("alunos")]
+        public Aluno GetAlunos()
+        {
+            return new Aluno
+            {
+                Nome = "Marcelo Amador",
+                Ativo = true,
+                DataCadastro = DateTime.Now,
+                Email = "marcelojaf@gmail.com",
+                Id = 1
+            };
+        }
+    }
+}
